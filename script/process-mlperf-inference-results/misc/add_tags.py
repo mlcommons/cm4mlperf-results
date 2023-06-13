ec3f7ec29bd8149af5c814acb33ec85c033b4618
Grigori Fursin <Grigori.Fursin@cTuning.org>
2023-06-13 11:09:18 +0200

    added general tag "mlperf" to inference, tiny and training to visualize all MLPerf experiments via CK playground

---
+++ script/process-mlperf-inference-results/misc/add_tags.py
@@ -0,0 +1,36 @@
+﻿import cmind
+
+def process():
+
+    r = cmind.access({'action':'find',
+                      'automation':'experiment'})
+
+    if r['return']>0: return r
+
+    lst = r['list']
+
+    for e in lst:
+        meta = e.meta
+        tags = meta.get('tags',[])
+
+        for t in ['mlperf-inference', 'mlperf-tiny', 'mlperf-training']:
+            if t in tags:
+                if 'mlperf' not in tags:
+                    tags.insert(0, 'mlperf')
+
+                    print (tags)
+                    
+                    meta['tags']=tags
+
+                    e.update(meta, replace = True)
+
+                    break
+
+
+    return {'return':0}
+
+
+if __name__ == "__main__":
+   r = process()
+   if r['return']>0: cmind.error(r)
+
